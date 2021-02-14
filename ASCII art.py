415b43fbcac1de987aacd90e9c500091fc8810ba
akashdeepgangatkar <69672474+akashdeepgangatkar@users.noreply.github.com>
2021-02-14 22:27:52 +0530

    Update ASCII art.py

--- ASCII art.py
+++ ASCII art.py
@@ -2,15 +2,15 @@ import pyfiglet
 data = pyfiglet.figlet_format('python')
 print(data)
 
-#changing fonts
+#Changing fonts
 data = pyfiglet.figlet_format('python', font = 'digital')
 print(data)
 
-#get list of fonts
+#Get list of fonts
 fonts = pyfiglet.FigletFont.getFonts()
 fonts = list(fonts)
 print("Total Number of Fonts available are : ",len(fonts))
  
-#print 10 font styles...
+#Print 10 font styles...
 for font in fonts[:10]:
     print(font)
